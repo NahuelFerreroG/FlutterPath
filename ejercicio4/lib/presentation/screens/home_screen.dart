50bb0f9d3888e1f897723d2267198e046f264be9
nahuel <nahuelferrero@gmail.com>
2023-05-12 19:13:16 -0300

    Ejercicio 4 - Terminado

--- ejercicio4/lib/presentation/screens/home_screen.dart
+++ ejercicio4/lib/presentation/screens/home_screen.dart
@@ -1,20 +1,47 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-class HomeScreen extends StatelessWidget {
+
+
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  
+  final randomImageIndex = Random();
+  late int imageIndex;
+
+  @override
+  void initState() {
+    imageIndex = randomImageIndex.nextInt(7)+1;
+    super.initState();
+  }
+ 
   @override
   Widget build(BuildContext context) {
 
     final colors = Theme.of(context).colorScheme;
-
+ 
     return Scaffold(
       appBar: AppBar(
         backgroundColor: colors.primary,
         title: const Text('HomeScreen with Image'),
         leading: const Icon(Icons.image_outlined),
       ),
-      body: const CustomImage(imageAsset: 'assets/images/sara.png')
+      body: CustomImage(imageAsset: 'assets/images/sara$imageIndex.png'),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.next_plan_outlined),
+        onPressed: (){
+          setState(() {
+            imageIndex = randomImageIndex.nextInt(7)+1;
+            
+          });
+        },
+      ),
     );
   }
 }
